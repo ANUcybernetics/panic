5da55b64b7cf2790e9045c86bb8aff1e723770a8
Ben Swift <ben@benswift.me>
2022-08-09 13:33:40 +1000

    add httpoison dep

--- mix.exs
+++ mix.exs
@@ -78,7 +78,8 @@ defmodule Panic.MixProject do
       {:earmark, "~> 1.4"},
       {:html_sanitize_ex, "~> 1.4"},
       {:nimble_totp, "~> 0.2.0"},
-      {:eqrcode, "~> 0.1.10"}
+      {:eqrcode, "~> 0.1.10"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
