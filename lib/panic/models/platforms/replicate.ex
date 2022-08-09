d36c29103566287722d89aee49c2cb4651d7bdbb
Ben Swift <ben@benswift.me>
2022-08-09 14:55:18 +1000

    add replicate functionality for (blocking) get prediction

--- lib/panic/models/platforms/replicate.ex
+++ lib/panic/models/platforms/replicate.ex
@@ -18,4 +18,41 @@ defmodule Panic.Models.Platforms.Replicate do
   def get_latest_model_version(model) do
     get_model_versions(model) |> List.last |> Map.get("id")
   end
+
+  def get_prediction_status(prediction_id) do
+    url = "#{@url}/predictions/#{prediction_id}"
+    case HTTPoison.get(url, @headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
+        {:ok, body} = Jason.decode(response_body)
+        body
+    end
+  end
+
+  def create_prediction(model, input_params) do
+    url = "#{@url}/predictions"
+    model_version = get_latest_model_version(model)
+
+    {:ok, request_body} = Jason.encode(%{version: model_version, input: input_params})
+
+    case HTTPoison.post(url, request_body, @headers) do
+      {:ok, %HTTPoison.Response{status_code: 201, body: response_body}} ->
+        {:ok, body} = Jason.decode(response_body)
+        return_prediction(body["id"])
+    end
+  end
+
+  def return_prediction(prediction_id) do
+    url = "#{@url}/predictions/#{prediction_id}"
+
+    case HTTPoison.get(url, @headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
+        {:ok, body} = Jason.decode(response_body)
+        case body do
+          %{"status" => "succeeded"} ->
+            body
+          %{"status" => status} when status in ~w(starting processing) ->
+            return_prediction(prediction_id)
+        end
+    end
+  end
 end
