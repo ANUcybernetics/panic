dd9ef0f3cdc0cbaab5ff98115cdd65435fc86da4
Ben Swift <ben@benswift.me>
2022-08-13 13:23:41 +1000

    make model_io only 1-in, 1-out
    for now... can generalise later

--- lib/panic/models.ex
+++ lib/panic/models.ex
@@ -30,10 +30,10 @@ defmodule Panic.Models do
   ## Examples
 
       iex> model_io("replicate:kuprel/min-dalle")
-      {[:text], [:image]}
+      {:text, :image}
   """
-  def model_io("replicate:kuprel/min-dalle"), do: {[:text], [:image]}
-  def model_io("replicate:rmokady/clip_prefix_caption"), do: {[:image], [:text]}
+  def model_io("replicate:kuprel/min-dalle"), do: {:text, :image}
+  def model_io("replicate:rmokady/clip_prefix_caption"), do: {:image, :text}
 
   @doc """
   Returns the list of runs.
