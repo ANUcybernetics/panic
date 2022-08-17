2d7a3d8b59c222dbd33f7784c951613fe1f0f325
Ben Swift <ben@benswift.me>
2022-08-17 18:43:19 +1000

    form component works

--- lib/panic_web/live/network_live/form_component.ex
+++ lib/panic_web/live/network_live/form_component.ex
@@ -4,12 +4,54 @@ defmodule PanicWeb.NetworkLive.FormComponent do
   alias Panic.{Models, Networks}
 
   @impl true
-  def update(%{run: run} = assigns, socket) do
-    changeset = Models.change_run(run)
+  def update(%{network: network} = assigns, socket) do
+    changeset = Networks.change_network(network)
 
     {:ok,
      socket
      |> assign(assigns)
      |> assign(:changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"network" => network_params}, socket) do
+    changeset =
+      socket.assigns.network
+      |> Networks.change_network(network_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"network" => network_params}, socket) do
+    case Networks.create_network(network_params) do
+      {:ok, _network} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Network created successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+    <.form
+    let={f}
+    for={@changeset}
+    id="new-network-form"
+    phx-target={@myself}
+    phx-change="validate"
+    phx-submit="save"
+    >
+    <.form_field type="text_input" form={f} field={:name} />
+    <.button type="submit" phx_disable_with="Saving..." label="Save" />
+    </.form>
+    </div>
+
+    """
   end
 end
