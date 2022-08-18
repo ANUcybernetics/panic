f961c66a06b3c0dfe36557a442eff30ac68182f2
Ben Swift <ben@benswift.me>
2022-08-18 10:17:17 +1000

    update sox conversion helper to deal with remote files as well

--- lib/panic/models/platforms/huggingface.ex
+++ lib/panic/models/platforms/huggingface.ex
@@ -38,7 +38,14 @@ defmodule Panic.Models.Platforms.HuggingFace do
   end
 
   def sox_convert_file(input_file, output_extension, sample_rate) do
-    output_file = "#{Path.rootname(input_file)}.#{output_extension}"
+    output_file = if String.match?(input_file, ~r/https?:\/\//) do
+      file = "#{@local_file_path}/#{Path.basename(input_file, Path.extname(input_file))}.#{output_extension}"
+      System.cmd("curl", ["--silent", "--output", file, input_file])
+      file
+    else
+      "#{Path.rootname(input_file)}.#{output_extension}"
+    end
+
     System.cmd("sox", [input_file, "#{output_file}", "rate", to_string(sample_rate)])
     output_file
   end
