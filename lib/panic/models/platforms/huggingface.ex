5e17e3a535d0594efabd22e1b09fb86e973648db
Ben Swift <ben@benswift.me>
2022-08-18 11:02:40 +1000

    handle "empty speech" case

--- lib/panic/models/platforms/huggingface.ex
+++ lib/panic/models/platforms/huggingface.ex
@@ -25,7 +25,7 @@ defmodule Panic.Models.Platforms.HuggingFace do
     input_file = sox_convert_file(audio_file, "flac", 16_000)
     {:ok, data} = File.read(input_file)
     {:ok, %{"text" => text}} = Jason.decode(create_and_wait(model, data))
-    String.downcase(text)
+    if text == "", do: "text-to-speech detected no speech", else: String.downcase(text)
   end
 
   defp headers do
