e0259f391938f858d18943bb5d2c9c419630c9f0
Ben Swift <ben@benswift.me>
2022-10-27 14:43:15 +1100

    refactor <.run_widget /> compontent into a new module
    this will be much better in the long run

--- lib/panic_web/live/network_live/archive.ex
+++ lib/panic_web/live/network_live/archive.ex
@@ -25,35 +25,4 @@ defmodule PanicWeb.NetworkLive.Archive do
      |> assign(:network, network)
      |> assign(:grouped_runs, grouped_runs)}
   end
-
-  def run_widget(assigns) do
-    ~H"""
-    <div class="relative block w-full text-center text-gray-800 bg-gray-200 shadow-lg dark:bg-gray-800 hover:bg-gray-300 dark:text-gray-400 dark:group-hover:text-gray-100">
-      <div class="h-48 grid place-items-center overflow-hidden">
-        <%= case Models.model_io(@run.model) do %>
-          <% {_, :text} -> %>
-            <div class="p-2 text-md text-left">
-              <%= @run.output %>
-            </div>
-          <% {_, :image} -> %>
-            <img class="w-full object-cover" src={@run.output} />
-          <% {_, :audio} -> %>
-            <audio autoplay controls={false} src={local_or_remote_url(@socket, @run.output)} />
-            <HeroiconsV1.Outline.volume_up class="w-12 h-12 mx-auto" />
-        <% end %>
-        <div class="absolute left-2 -bottom-6 text-xs">
-          <%= @run.model |> String.split(~r/[:\/]/) |> List.last() %>
-        </div>
-      </div>
-    </div>
-    """
-  end
-
-  def local_or_remote_url(socket, url) do
-    if String.match?(url, ~r/https?:\/\//) do
-      url
-    else
-      Routes.static_path(socket, "/model_outputs/" <> Path.basename(url))
-    end
-  end
 end
--- lib/panic_web/live/network_live/show.ex
+++ lib/panic_web/live/network_live/show.ex
@@ -163,54 +163,6 @@ defmodule PanicWeb.NetworkLive.Show do
      )}
   end
 
-  def run_widget(assigns) do
-    ~H"""
-    <div class="relative block w-full text-center text-gray-800 bg-gray-200 shadow-lg dark:bg-gray-800 hover:bg-gray-300 dark:text-gray-400 dark:group-hover:text-gray-100">
-      <div class="h-48 grid place-items-center overflow-hidden">
-        <%= if @run do %>
-          <%= case @run.status do %>
-            <% :created -> %>
-              <HeroiconsV1.Outline.minus_circle class="w-12 h-12 mx-auto" />
-            <% :running -> %>
-              <.spinner class="mx-auto" size="md" />
-            <% :succeeded -> %>
-              <%= case Models.model_io(@run.model) do %>
-                <% {_, :text} -> %>
-                  <div class="p-2 text-md text-left">
-                    <%= for line <- String.split(@run.output, "\n\n") do %>
-                      <%= unless line == "" do %>
-                        <p><%= line %></p>
-                      <% end %>
-                    <% end %>
-                  </div>
-                <% {_, :image} -> %>
-                  <img class="w-full object-cover" src={@run.output} />
-                <% {_, :audio} -> %>
-                  <audio autoplay controls={false} src={local_or_remote_url(@socket, @run.output)} />
-                  <HeroiconsV1.Outline.volume_up class="w-12 h-12 mx-auto" />
-              <% end %>
-            <% :failed -> %>
-              <HeroiconsV1.Outline.x_circle class="w-12 h-12 mx-auto" />
-          <% end %>
-          <div class="absolute left-2 -bottom-6">
-            <%= @run.model |> String.split(~r/[:\/]/) |> List.last() %>
-          </div>
-        <% else %>
-          <span class="text-gray-400 italic">blank</span>
-        <% end %>
-      </div>
-    </div>
-    """
-  end
-
-  def local_or_remote_url(socket, url) do
-    if String.match?(url, ~r/https?:\/\//) do
-      url
-    else
-      Routes.static_path(socket, "/model_outputs/" <> Path.basename(url))
-    end
-  end
-
   defp cycle_index(_cycle, %Run{parent_id: nil}), do: 0
 
   defp cycle_index(cycle, %Run{parent_id: parent_id}) do
