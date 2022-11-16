cf032d48d98c1479ba43a65e07d281cf37fdcf54
Ben Swift <ben@benswift.me>
2022-11-16 17:20:47 +1100

    add some filthy "create a socy user" code to migrations

--- lib/panic/release.ex
+++ lib/panic/release.ex
@@ -5,6 +5,10 @@ defmodule Panic.Release do
   """
   @app :panic
 
+  # gross! gross! gross! but deadlines.
+  alias Panic.Accounts
+  alias Panic.Accounts.UserSeeder
+
   def migrate do
     load_app()
 
@@ -24,5 +28,21 @@ defmodule Panic.Release do
 
   defp load_app do
     Application.load(@app)
+  end
+
+  def maybe_create_user do
+    unless Accounts.get_user_by_email("socy@anu.edu.au") do
+      # this is stolen from seeds.exs, but that doesn't run in prod, so again,
+      # it'll do for the exhibition
+      normal_user =
+        UserSeeder.normal_user(%{
+              email: "socy@anu.edu.au",
+              name: "Panic Viewer",
+              password: "cybernetics",
+              confirmed_at: Timex.now() |> Timex.to_naive_datetime()})
+
+      org = Panic.Orgs.list_orgs() |> List.first()
+      Panic.Orgs.create_invitation(org, %{email: normal_user.email})
+    end
   end
 end
