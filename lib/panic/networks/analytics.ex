01cb01e83a4097aa6aa9ee35a87dfb91af989616
Ben Swift <ben@benswift.me>
2022-12-06 13:48:14 +1100

    add time_to_word

--- lib/panic/networks/analytics.ex
+++ lib/panic/networks/analytics.ex
@@ -6,6 +6,26 @@ defmodule Panic.Networks.Analytics do
   alias Panic.Models.Run
   alias Panic.Networks.Network
 
+
+  def time_to_word(%Network{id: id}, ""), do: {0, 0, 0}
+
+  def time_to_word(%Network{id: id}, word) do
+    num_runs = Repo.aggregate((from r in Run, where: r.network_id == ^id), :count)
+
+    word_like = "%" <> word <> "%"
+
+    query = from r in Run, where: r.network_id == ^id, where: like(r.output, ^word_like), group_by: r.first_run_id, select: {r.first_run_id, min(r.cycle_index)}
+
+    cycle_lengths = Repo.all(query)
+
+    avg_tts =
+      cycle_lengths
+      |> Enum.map(fn {_first_run_id, cycle_length} -> cycle_length end)
+      |> mean()
+
+    {num_runs, Enum.count(cycle_lengths) / num_runs, avg_tts}
+  end
+
   def average_cycle_length(%Network{id: id}) do
     query = from r in Run, where: r.network_id == ^id, select: count(), group_by: r.first_run_id
 
