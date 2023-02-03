c7b764a9ffd7d0f1257b957fc2274b53481f9d29
Ben Swift <ben@benswift.me>
2023-02-03 14:53:45 +1100

    use network.id rather than "network:#{network.id}" as the FSM identifier
    might as well; it's a bijection

--- test/panic/run_fsm_test.exs
+++ test/panic/run_fsm_test.exs
@@ -7,32 +7,29 @@ defmodule Panic.RunFSMTest do
     setup [:create_network, :load_env_vars]
 
     test "single user, golden path", %{network: network} do
-      fsm_name = "network:#{network.id}"
-
-      Finitomata.start_fsm(Panic.Runs.RunFSM, fsm_name, %{network: network})
-      assert Finitomata.alive?(fsm_name)
-      assert %Finitomata.State{current: :waiting} = Finitomata.state(fsm_name)
+      Finitomata.start_fsm(Panic.Runs.RunFSM, network.id, %{network: network})
+      assert Finitomata.alive?(network.id)
+      assert %Finitomata.State{current: :waiting} = Finitomata.state(network.id)
 
       ## genesis input
-      send_event_and_sleep(fsm_name, {:input, "ok, let's kick things off..."})
-      assert %Finitomata.State{current: :running} = Finitomata.state(fsm_name)
+      send_event_and_sleep(network.id, {:input, "ok, let's kick things off..."})
+      assert %Finitomata.State{current: :running} = Finitomata.state(network.id)
 
-      Process.sleep(10_000)
+      Process.sleep(30_000)
 
-      IO.inspect(Finitomata.state(fsm_name))
+      send_event_and_sleep(network.id, {:reset, nil})
+      assert %Finitomata.State{current: :waiting} = Finitomata.state(network.id)
 
-      send_event_and_sleep(fsm_name, {:reset, nil})
-      assert %Finitomata.State{current: :waiting} = Finitomata.state(fsm_name)
-
-      send_event_and_sleep(fsm_name, {:shut_down, nil})
+      IO.inspect("shutting things down...")
+      send_event_and_sleep(network.id, {:shut_down, nil})
       ## here, at the end of all things
-      assert not Finitomata.alive?(fsm_name)
+      assert not Finitomata.alive?(network.id)
     end
   end
 
   # helper function for testing FSMs (because it takes a bit for them to finish transitioning)
-  defp send_event_and_sleep(fsm_name, event, sleep_dur \\ 200) do
-    Finitomata.transition(fsm_name, event)
+  defp send_event_and_sleep(network_id, event, sleep_dur \\ 200) do
+    Finitomata.transition(network_id, event)
     Process.sleep(sleep_dur)
   end
 
