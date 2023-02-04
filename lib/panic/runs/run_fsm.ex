c920ea8974ebf219e8a8f2f240771103ab0103a0
Ben Swift <ben@benswift.me>
2023-02-04 16:49:28 +1100

    update "should I drop this input, or keep going with the run logic

--- lib/panic/runs/run_fsm.ex
+++ lib/panic/runs/run_fsm.ex
@@ -8,10 +8,10 @@ defmodule Panic.Runs.RunFSM do
   pre_run --> |init!| waiting
   waiting --> |input| running
   running --> |input| running
-  waiting --> |pause| waiting
-  running --> |pause| waiting
-  waiting --> |reset| waiting
   running --> |reset| waiting
+  waiting --> |lock| locked
+  running --> |lock| locked
+  locked -->  |unlock| running
   waiting --> |shut_down| post_run
   running --> |shut_down| post_run
   """
@@ -22,24 +22,16 @@ defmodule Panic.Runs.RunFSM do
 
   @impl Finitomata
   def on_transition(:pre_run, :init!, _event_payload, payload) do
-    {:ok, :waiting,
-     payload
-     # this needs to be *well* in the past (because clocks might not be synced between processes)
-     |> Map.put(:lockout_time, NaiveDateTime.add(NaiveDateTime.utc_now(), -1, :day))}
-  end
-
-  @impl Finitomata
-  def on_transition(_state, :reset, _event_payload, payload) do
-    {:ok, :waiting, %{payload | lockout_time: NaiveDateTime.utc_now()}}
+    {:ok, :waiting, Map.put(payload, :last_input, nil)}
   end
 
   @impl Finitomata
   def on_transition(state, :input, input, %{network: network} = payload) do
-    if locked_out?(input, payload) do
-      {:ok, state, payload}
-    else
-      _task =
-        Task.Supervisor.start_child(Panic.Platforms.TaskSupervisor, fn ->
+    ## first 10 cycles is the "lockout period"
+    if next_in_run?(input, payload.last_input) do
+      Task.Supervisor.start_child(
+        Panic.Platforms.TaskSupervisor,
+        fn ->
           {:ok, prediction} =
             case input do
               input when is_binary(input) ->
@@ -50,20 +42,39 @@ defmodule Panic.Runs.RunFSM do
             end
 
           Finitomata.transition(network.id, {:input, prediction})
-        end)
+        end,
+        restart: :transient
+      )
 
-      if state == :waiting do
-        {:ok, :running,
-         %{payload | lockout_time: NaiveDateTime.utc_now() |> NaiveDateTime.add(30)}}
-      else
-        {:ok, :running, payload}
-      end
+      {:ok, :running, %{payload | last_input: input}}
+    else
+      {:ok, state, payload}
     end
   end
 
-  defp locked_out?(%Prediction{run_index: 0}, %{lockout_time: lockout_time}) do
-    NaiveDateTime.utc_now() < lockout_time
+  @impl Finitomata
+  def on_transition(state, :lock, duration_in_seconds, payload) do
+    Finitomata.transition(payload.network.id, {:unlock, state}, duration_in_seconds * 1000)
+    {:ok, :locked, payload}
   end
 
-  defp locked_out?(_input, _payload), do: false
+  @impl Finitomata
+  def on_transition(_state, :unlock, _prev_state, payload) do
+    ## TODO figure out how to re-start the run if prev_state == :running
+    {:ok, :waiting, payload}
+  end
+
+  @impl Finitomata
+  def on_transition(_state, :reset, _event_payload, payload) do
+    {:ok, :waiting, payload}
+  end
+
+  defp next_in_run?(input, last_input) do
+    case {input, last_input} do
+      {input, nil} when is_binary(input) -> true
+      {%Prediction{run_index: 0}, <<>>} -> true
+      {%Prediction{run_index: current}, %Prediction{run_index: last}} -> current == last + 1
+      _ -> false
+    end
+  end
 end
