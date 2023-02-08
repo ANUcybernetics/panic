3d74bdcd164b5e129f8ec85c892d3fe4fe0d9309
Ben Swift <ben@benswift.me>
2023-02-08 21:19:26 +1100

    add create user, log in and set up network & tokens helpers to api token live tests

--- test/panic_web/live/api_token_live_test.exs
+++ test/panic_web/live/api_token_live_test.exs
@@ -2,19 +2,39 @@ defmodule PanicWeb.APITokenLiveTest do
   use PanicWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  import Panic.NetworksFixtures
   import Panic.AccountsFixtures
 
   @create_attrs %{name: "some name", token: "some token"}
   @update_attrs %{name: "some updated name", token: "some updated token"}
   @invalid_attrs %{name: nil, token: nil}
 
-  defp create_api_token(_) do
-    api_token = api_token_fixture()
-    %{api_token: api_token}
+  defp create_and_log_in_user(%{conn: conn} = context) do
+    password = "123456789abcd"
+    user = user_fixture(%{password: password})
+
+    {:ok, lv, _html} = live(conn, ~p"/users/log_in")
+
+    form =
+      form(lv, "#login_form", user: %{email: user.email, password: password, remember_me: true})
+
+    conn = submit_form(form, conn)
+
+    context
+    |> Map.put(:user, user)
+    |> Map.put(:conn, conn)
+  end
+
+  defp create_network(%{user: user} = context) do
+    Map.put(context, :network, network_fixture(%{user_id: user.id}))
+  end
+
+  defp create_api_token(%{user: user} = context) do
+    Map.put(context, :api_token, api_token_fixture(%{user_id: user.id}))
   end
 
   describe "Index" do
-    setup [:create_api_token]
+    setup [:create_and_log_in_user, :create_network, :create_api_token]
 
     test "lists all api_token", %{conn: conn, api_token: api_token} do
       {:ok, _index_live, html} = live(conn, ~p"/api_tokens")
@@ -76,7 +96,7 @@ defmodule PanicWeb.APITokenLiveTest do
   end
 
   describe "Show" do
-    setup [:create_api_token]
+    setup [:create_and_log_in_user, :create_network, :create_api_token]
 
     test "displays api_token", %{conn: conn, api_token: api_token} do
       {:ok, _show_live, html} = live(conn, ~p"/api_tokens/#{api_token}")
