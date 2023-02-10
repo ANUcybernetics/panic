d8591c3deb0665db5c0d9471abb51166927f744d
Ben Swift <ben@benswift.me>
2023-02-10 18:01:56 +1100

    predictions show assigns also needs the network
    (alternately, we could get it from the prediction... hmm)

--- lib/panic_web/live/prediction_live/show.ex
+++ lib/panic_web/live/prediction_live/show.ex
@@ -2,6 +2,7 @@ defmodule PanicWeb.PredictionLive.Show do
   use PanicWeb, :live_view
 
   alias Panic.Predictions
+  alias Panic.Networks
 
   @impl true
   def mount(_params, _session, socket) do
@@ -9,10 +10,13 @@ defmodule PanicWeb.PredictionLive.Show do
   end
 
   @impl true
-  def handle_params(%{"id" => id}, _, socket) do
+  def handle_params(%{"network_id" => network_id, "id" => id}, _, socket) do
+    network = network_id |> String.to_integer() |> Networks.get_network!()
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:network, network)
      |> assign(:prediction, Predictions.get_prediction!(id))}
   end
 
