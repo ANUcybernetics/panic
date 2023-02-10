d900030e941b60b55b69c8d20a458bf6142b9e51
Ben Swift <ben@benswift.me>
2023-02-10 21:52:11 +1100

    remove dead view test

--- test/panic_web/controllers/page_controller_test.exs
+++ test/panic_web/controllers/page_controller_test.exs
@@ -1,8 +1,5 @@
 defmodule PanicWeb.PageControllerTest do
   use PanicWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
+  ## no dead views means no dead view tests
 end
