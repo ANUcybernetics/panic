52d8a3333d2f6b2136da3bd1a6b79111d7f897c2
Ben Swift <ben@benswift.me>
2023-02-12 20:47:33 +1100

    update latest -> permalink in route
    not implemented yet

--- lib/panic_web/router.ex
+++ lib/panic_web/router.ex
@@ -77,8 +77,8 @@ defmodule PanicWeb.Router do
       ## "running grid" view
       live "/networks/:id", NetworkLive.Show, :show
 
-      # redirect to the latest network (although not by-user, so I'm not sure this is going to work)
-      live "/networks/latest", NetworkLive.Index, :latest
+      # should be an option for networks to be given a (public) permalink
+      live "/networks/permalink", NetworkLive.Index, :latest
       ## also accepts query params for screen/grid_mod and will live update as
       ## new predictions come in
       live "/networks/:network_id/predictions/:id", PredictionLive.Show, :show
