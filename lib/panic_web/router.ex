edb197c133096c22a223853ba6ddb1375dbf0fea
Ben Swift <ben@benswift.me>
2023-02-20 16:48:38 +1100

    update permalink route
    the logic for this isn't implemented, anyway

--- lib/panic_web/router.ex
+++ lib/panic_web/router.ex
@@ -87,8 +87,8 @@ defmodule PanicWeb.Router do
       ## "running grid" view
       live "/networks/:id", NetworkLive.Show, :show
 
-      # should be an option for networks to be given a (public) permalink
-      live "/networks/permalink", NetworkLive.Index, :latest
+      # should be an option for a user to have networks to be given a (public) permalink
+      live "/networks/permalinks/:id", NetworkLive.Show, :permalink
 
       # the "terminal"
       live "/networks/:network_id/predictions/new", PredictionLive.Index, :new
