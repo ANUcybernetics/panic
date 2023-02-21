986f6b35589497ced65fb98ff2e13e75d36cdaf1
Ben Swift <ben@benswift.me>
2023-02-21 11:40:54 +1100

    bugfix: controller code wasn't updated to match the router

--- lib/panic_web/live/prediction_live/show.ex
+++ lib/panic_web/live/prediction_live/show.ex
@@ -10,14 +10,14 @@ defmodule PanicWeb.PredictionLive.Show do
   end
 
   @impl true
-  def handle_params(%{"network_id" => network_id, "id" => id}, _, socket) do
+  def handle_params(%{"network_id" => network_id, "prediction_id" => prediction_id}, _, socket) do
     network = Networks.get_network!(network_id)
 
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:network, network)
-     |> assign(:prediction, Predictions.get_prediction!(id))}
+     |> assign(:prediction, Predictions.get_prediction!(prediction_id))}
   end
 
   defp page_title(:show), do: "Show Prediction"
