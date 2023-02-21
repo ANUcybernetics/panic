f09edcc99ac7ad87ef2f9879b0a8647802211e24
Ben Swift <ben@benswift.me>
2023-02-21 17:11:07 +1100

    update prediction_fixture/1 so that it returns a valid(ish) genesis prediction

--- test/support/fixtures/predictions_fixtures.ex
+++ test/support/fixtures/predictions_fixtures.ex
@@ -5,9 +5,15 @@ defmodule Panic.PredictionsFixtures do
   """
 
   import Panic.NetworksFixtures
+  alias Panic.Predictions
+  alias Panic.Predictions.Prediction
 
   @doc """
-  Generate a prediction.
+  Generate a (genesis) prediction.
+
+  This is expecting a map of fake data; it won't be a genesis prediction, and
+  as a "next" prediction there won't be any preceeding ones.
+
   """
   def prediction_fixture(attrs \\ %{}) do
     network = network_fixture()
@@ -17,14 +23,16 @@ defmodule Panic.PredictionsFixtures do
         %{
           input: "some input",
           metadata: %{},
-          model: "some model",
+          model: "replicate:stability-ai/stable-diffusion",
           output: "some output",
-          run_index: 42,
+          run_index: 0,
           network_id: network.id
         },
         attrs
       )
-      |> Panic.Predictions.create_prediction_from_attrs()
+      |> Predictions.create_prediction_from_attrs()
+
+    {:ok, prediction} = Predictions.update_prediction(prediction, %{genesis_id: prediction.id})
 
     prediction
   end
