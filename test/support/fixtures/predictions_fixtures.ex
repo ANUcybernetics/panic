622e087fa044d2150da3ea050ec6b7a89e5c1b6f
Ben Swift <ben@benswift.me>
2023-03-10 13:15:55 +1100

    refactor prediction fixtures again
    now it's always fake, never makes a real API call

--- test/support/fixtures/predictions_fixtures.ex
+++ test/support/fixtures/predictions_fixtures.ex
@@ -8,36 +8,23 @@ defmodule Panic.PredictionsFixtures do
   alias Panic.Predictions
 
   @doc """
-  Generate a (genesis) prediction.
-
-  Unless `Panic.Platforms.api_call/3` is mocked in the calling context, this
-  will make a real platform API call.
-
+  Generate a (genesis) prediction fixture.
   """
   def genesis_prediction_fixture(%Panic.Networks.Network{} = network) do
-    tokens = Panic.Accounts.get_api_token_map(network.user_id)
+    changeset = Predictions.Prediction.genesis_changeset("some input", network)
 
-    {:ok, prediction} = Predictions.create_genesis_prediction("some input", network, tokens)
+    {:ok, prediction} =
+      changeset.params
+      |> Map.put("output", "some output")
+      |> Predictions.create_prediction()
 
     prediction
   end
 
   @doc """
-  Generate a (genesis) prediction.
+  Generate a prediction fixture from attrs.
 
-  Unless `Panic.Platforms.api_call/3` is mocked in the calling context, this
-  will make a real platform API call.
-
-  """
-  def genesis_prediction_fixture() do
-    network_fixture()
-    |> genesis_prediction_fixture()
-  end
-
-  @doc """
-  Generate a prediction from attrs.
-
-  This doesn't check if it's a valid genesis/next prediciton, and expects you to
+  This doesn't check if it's a valid genesis/next prediction, and expects you to
   do all the leg-work to pass in appropriate `attrs` for your use-case.
 
   """
