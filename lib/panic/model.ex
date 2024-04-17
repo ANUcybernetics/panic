acbbc02009476a79c6d766de16884bc7f9d3e741
Ben Swift <ben@benswift.me>
2024-04-17 16:02:19 +1000

    update the Panic.Model behaviour

--- lib/panic/model.ex
+++ lib/panic/model.ex
@@ -1,3 +1,13 @@
 defmodule Panic.Model do
+  @doc """
+  Return the model information.
+  """
+  @callback info() :: Panic.Models.ModelInfo.t()
 
+  @doc """
+  Invoke the model with the given input.
+
+  This is a blocking function.
+  """
+  @callback invoke(String.t()) :: {:ok, String.t()} | {:error, String.t()}
 end
