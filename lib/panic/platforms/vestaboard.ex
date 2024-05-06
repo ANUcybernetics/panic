e83bacff47ad3cdec79dedaca78ca4c332f1932d
Ben Swift <ben@benswift.me>
2024-05-06 14:08:39 +1000

    update Vestaboard to use the RW API
    this code is mostly pulled from the v2-hotfix branch

--- lib/panic/platforms/vestaboard.ex
+++ lib/panic/platforms/vestaboard.ex
@@ -1,56 +1,35 @@
 defmodule Panic.Models.Platforms.Vestaboard do
-  @url "https://platform.vestaboard.com"
+  @url "https://rw.vestaboard.com/"
 
-  def list_subscriptions(board_name, tokens) do
-    Finch.build(:get, "#{@url}/subscriptions", headers(board_name, tokens))
-    |> Finch.request(Panic.Finch)
-    |> case do
-      {:ok, %Finch.Response{body: response_body, status: 200}} ->
-        Jason.decode!(response_body)
+  def send_text(board_name, text) do
+    request_body = Jason.encode!(%{"text" => text})
+
+    headers = [
+      {"X-Vestaboard-Read-Write-Key", api_key(board_name)},
+      {"Content-Type", "application/json"}
+    ]
+
+    case Req.post(@url, body: request_body, headers: headers) do
+      {:ok, %Req.Response{status: 200, body: body}} ->
+        Jason.decode(body)
+
+      {:ok, %Req.Response{status: status_code}} when status_code in [400, 405] ->
+        {:error, :bad_request}
+
+      {:ok, %Req.Response{status: 304}} ->
+        {:error, :not_modified}
+
+      {:ok, %Req.Response{status: 503}} ->
+        {:error, :too_many_requests}
     end
   end
 
-  def send_text(board_name, text, tokens) do
-    url = "#{@url}/subscriptions/#{subscription_id(board_name, tokens)}/message"
-
-    Finch.build(:post, url, headers(board_name, tokens), Jason.encode!(%{text: text}))
-    |> Finch.request(Panic.Finch)
-    |> case do
-      {:ok, %Finch.Response{body: response_body, status: 200}} ->
-        Jason.decode(response_body)
-
-      {:ok, %Finch.Response{status: 503}} ->
-        # this isn't *really* ok, but hopefully we can ignore it
-        {:ok, :too_many_requests}
-    end
+  def clear(board_name) do
+    # this is a workaround, because the RW API doesn't allow blank messages
+    send_text(board_name, "blank")
   end
 
-  def clear_all(board_ids, tokens) do
-    board_ids |> Enum.each(&send_text(&1, "", tokens))
-  end
-
-  ## NOTE: Vestaboard API requires heaps of info: board & subscription IDs,
-  ## plus an API key *and* an API secret, and we (somewhat jankily) store them
-  ## in one DB field using `:` separators, because I *think* the tokens
-  ## themselves will never contain a `:`
-  defp subscription_id(board_name, tokens) do
-    %{^board_name => token} = tokens
-    [_board_id, subscription_id, _api_key, _api_secret] = String.split(token, ":")
-    subscription_id
-  end
-
-  defp headers(board_name, tokens) do
-    ## NOTE: Vestaboard API requires heaps of info: board & subscription IDs,
-    ## plus an API key *and* an API secret, and we (somewhat jankily) store them
-    ## in one DB field using `:` separators, because I *think* the tokens
-    ## themselves will never contain a `:`
-    %{^board_name => token} = tokens
-    [_board_id, _subscription_id, api_key, api_secret] = String.split(token, ":")
-
-    %{
-      "X-Vestaboard-Api-Key" => api_key,
-      "X-Vestaboard-Api-Secret" => api_secret,
-      "Content-Type" => "application/json"
-    }
+  defp api_key(board_name) do
+    Application.fetch_env!(:panic, :"vestaboard_api_token_#{to_string(board_name)}")
   end
 end
