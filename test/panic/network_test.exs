333cd9189ecfd1cbc7508c2928a2d1d234078a7d
Ben Swift <ben@benswift.me>
2024-06-18 15:10:47 +1000

    remove the non-property tests of Network resource

--- test/panic/network_test.exs
+++ test/panic/network_test.exs
@@ -2,7 +2,6 @@ defmodule Panic.NetworkTest do
   use Panic.DataCase
   use ExUnitProperties
   alias Panic.Engine.Network
-  alias Panic.Models
 
   describe "CRUD actions" do
     # now if our action inputs are invalid when we think they should be valid, we will find out here
@@ -44,9 +43,13 @@ defmodule Panic.NetworkTest do
       end
     end
 
+    # TODO what's the best way with property testing to test that it gives the right invalid changeset on invalid input?
+
     property "Network read action" do
       check all(network <- Panic.Generators.network()) do
         assert network.id == Panic.Engine.get_network!(network.id).id
+        # there shouldn't ever be a negative ID in the db, so this should always raise
+        assert_raise Ash.Error.Invalid, fn -> Ash.get!(Network, -1) end
       end
     end
 
@@ -58,63 +61,6 @@ defmodule Panic.NetworkTest do
         network = Panic.Engine.set_state!(network.id, state)
         assert network.state == state
       end
-    end
-  end
-
-  describe "Panic.Engine.Network resource" do
-    test "changeset for :create action with valid data creates a network" do
-      valid_attrs = %{
-        name: "My Network",
-        description: "A super cool network",
-        models: [
-          # TODO change this to an actual model module once they exist
-          Models.GPT4o,
-          Models.SDXL,
-          Models.LLaVA
-        ]
-      }
-
-      network =
-        Network
-        |> Ash.Changeset.for_create(:create, valid_attrs)
-        |> Ash.create!()
-
-      assert network.name == valid_attrs.name
-      assert network.description == valid_attrs.description
-      assert network.models == valid_attrs.models
-      assert network.state == :stopped
-    end
-
-    test "code interface for :create action with valid data creates a network" do
-      valid_attrs = %{
-        name: "My Network",
-        description: "A super cool network",
-        models: [
-          # TODO change this to an actual model module once they exist
-          Panic.Engine
-        ]
-      }
-
-      network =
-        Panic.Engine.create_network!(
-          valid_attrs.name,
-          valid_attrs.description,
-          valid_attrs.models
-        )
-
-      assert network.name == valid_attrs.name
-      assert network.description == valid_attrs.description
-      assert network.models == valid_attrs.models
-      assert network.state == :stopped
-    end
-
-    test "raise if there's no Network with a given id" do
-      assert_raise Ash.Error.Invalid, fn -> Ash.get!(Network, 1234) end
-    end
-
-    test "create action with invalid data returns error changeset" do
-      assert {:error, %Ash.Error.Invalid{}} =
-               Panic.Engine.create_network("Good name", "Good description", [BadModule])
     end
   end
 
