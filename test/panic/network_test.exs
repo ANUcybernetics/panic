825ee509d318da02c4050d32262a38182b3ee89d
Ben Swift <ben@benswift.me>
2024-06-25 16:51:23 +1000

    update Network tests to all require a user (actor)

--- test/panic/network_test.exs
+++ test/panic/network_test.exs
@@ -6,13 +6,15 @@ defmodule Panic.NetworkTest do
   describe "CRUD actions" do
     # now if our action inputs are invalid when we think they should be valid, we will find out here
     property "accepts all valid input" do
+      user = Panic.Generators.user_fixture()
+
       check all(input <- input_for_create()) do
         assert %Ash.Changeset{valid?: true} =
                  Panic.Engine.changeset_to_create_network(
                    input.name,
                    input.description,
                    input.models,
-                   authorize?: false
+                   actor: user
                  )
       end
     end
@@ -33,21 +35,25 @@ defmodule Panic.NetworkTest do
     end
 
     property "succeeds on all valid input" do
+      user = Panic.Generators.user_fixture()
+
       check all(input <- input_for_create()) do
         Network
-        |> Ash.Changeset.for_create(:create, input)
+        |> Ash.Changeset.for_create(:create, input, actor: user)
         |> Ash.create!()
       end
     end
 
     property "succeeds on all valid input (code interface version)" do
+      user = Panic.Generators.user_fixture()
+
       check all(input <- input_for_create()) do
         {:ok, network} =
           Panic.Engine.create_network(
             input.name,
             input.description,
             input.models,
-            authorize?: false
+            actor: user
           )
 
         assert network.name == input.name
@@ -61,7 +67,9 @@ defmodule Panic.NetworkTest do
     # TODO what's the best way with property testing to test that it gives the right invalid changeset on invalid input?
 
     property "Network read action" do
-      check all(network <- Panic.Generators.network()) do
+      user = Panic.Generators.user_fixture()
+
+      check all(network <- Panic.Generators.network(user)) do
         assert network.id == Panic.Engine.get_network!(network.id).id
         # there shouldn't ever be a negative ID in the db, so this should always raise
         assert_raise Ash.Error.Invalid, fn -> Ash.get!(Network, -1) end
@@ -69,8 +77,10 @@ defmodule Panic.NetworkTest do
     end
 
     property "Network set_state action" do
+      user = Panic.Generators.user_fixture()
+
       check all(
-              network <- Panic.Generators.network(),
+              network <- Panic.Generators.network(user),
               state <- member_of([:starting, :running, :paused, :stopped])
             ) do
         network = Panic.Engine.set_state!(network.id, state)
