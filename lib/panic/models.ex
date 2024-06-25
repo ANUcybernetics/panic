479d20fa27149ccad69d54f9fb8e324250550dd0
Ben Swift <ben@benswift.me>
2024-06-25 16:38:14 +1000

    add Panic.Models.list()

---
+++ lib/panic/models.ex
@@ -0,0 +1,16 @@
+defmodule Panic.Models do
+  def list do
+    :code.all_loaded()
+    |> Enum.filter(fn {mod, _} -> implements_model(mod) end)
+    |> Enum.map(fn {mod, _} -> mod end)
+  end
+
+  defp implements_model(module) do
+    if function_exported?(module, :__info__, 1) do
+      behaviours = Keyword.get(module.__info__(:attributes), :behaviour, [])
+      Panic.Model in behaviours
+    else
+      false
+    end
+  end
+end
--- test/panic/models_test.exs
+++ test/panic/models_test.exs
@@ -3,6 +3,27 @@ defmodule Panic.ModelsTest do
   alias Panic.Engine.Network
   alias Panic.Engine.Invocation
 
+  describe "model helpers" do
+    test "list all modules which conform to Model behaviour" do
+      models = [
+        Panic.Models.BLIP2,
+        Panic.Models.StableDiffusion,
+        Panic.Models.ClipCaptionReward,
+        Panic.Models.GPT4o,
+        Panic.Models.CogPromptParrot,
+        Panic.Models.GPT4Turbo,
+        Panic.Models.LLaVA,
+        Panic.Models.ClipPrefixCaption,
+        Panic.Models.Vicuna13B,
+        Panic.Models.SDXL,
+        Panic.Models.GPT4
+      ]
+
+      # check that they're all in the list
+      assert Panic.Models.list() |> MapSet.new() == MapSet.new(models)
+    end
+  end
+
   describe "Panic.Engine.Invocation resource" do
     test "changeset for :create_first action with valid data creates an invocation" do
       network = network_fixture()
