27d480f366e2ce3544fc7f831ab9667e64bd307f
Ben Swift <ben@benswift.me>
2024-08-16 13:06:39 +1000

    create UserLive modules using ash live generator

---
+++ lib/panic_web/live/user_live/show.ex
@@ -0,0 +1,55 @@
+defmodule PanicWeb.UserLive.Show do
+  use PanicWeb, :live_view
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <.header>
+      User <%= @user.id %>
+      <:subtitle>This is a user record from your database.</:subtitle>
+
+      <:actions>
+        <.link patch={~p"/users/#{@user}/edit"} phx-click={JS.push_focus()}>
+          <.button>Edit user</.button>
+        </.link>
+      </:actions>
+    </.header>
+
+    <.list>
+      <:item title="Id"><%= @user.id %></:item>
+
+      <:item title="Email"><%= @user.email %></:item>
+    </.list>
+
+    <.back navigate={~p"/users"}>Back to users</.back>
+
+    <.modal :if={@live_action == :edit} id="user-modal" show on_cancel={JS.patch(~p"/users/#{@user}")}>
+      <.live_component
+        module={PanicWeb.UserLive.FormComponent}
+        id={@user.id}
+        title={@page_title}
+        action={@live_action}
+        current_user={@current_user}
+        user={@user}
+        patch={~p"/users/#{@user}"}
+      />
+    </.modal>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:user, Ash.get!(Panic.Accounts.User, id, actor: socket.assigns.current_user))}
+  end
+
+  defp page_title(:show), do: "Show User"
+  defp page_title(:edit), do: "Edit User"
+end
