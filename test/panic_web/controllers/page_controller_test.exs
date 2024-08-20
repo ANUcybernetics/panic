daeb40863fef231ad95be2be5ce85a3ac3d0566c
Ben Swift <ben@benswift.me>
2024-08-20 17:15:26 +1000

    remove the PageController test
    because I removed that page. liveview all the way, baby.

--- test/panic_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PanicWeb.PageControllerTest do
-  use PanicWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Panic is live."
-  end
-end
