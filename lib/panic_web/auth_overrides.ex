5974de87920304aa07db062b463d55b406c77694
Ben Swift <ben@benswift.me>
2024-08-20 13:52:54 +1000

    add auth_overrides
    workaround for this issue: https://github.com/team-alembic/ash_authentication_phoenix/issues/497

--- lib/panic_web/auth_overrides.ex
+++ lib/panic_web/auth_overrides.ex
@@ -6,4 +6,9 @@ defmodule PanicWeb.AuthOverrides do
     set :register_toggle_text, nil
     set :reset_toggle_text, nil
   end
+
+  override Components.Password.Input do
+    set :password_input_label, "password"
+    set :identity_input_label, "email"
+  end
 end
