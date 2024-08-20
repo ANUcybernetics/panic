7d91e0067584973b7f44b8ba6fed875fcdd6313a
Ben Swift <ben@benswift.me>
2024-08-20 17:13:20 +1000

    add working PhoenixTest tests
    with a logged in user and everything

--- test/panic_web/live/user_live_test.exs
+++ test/panic_web/live/user_live_test.exs
@@ -1,17 +1,51 @@
-defmodule PanicWeb.UserLiveTest do
-  use PanicWeb.ConnCase
+defmodule PanicWeb.UserDashboardLiveTest do
+  @moduledoc """
+  Test the user dashboard
 
-  describe "user can" do
-    test "log in", %{conn: conn} do
-      password = "abcd1234"
-      user = Panic.Fixtures.user(password)
+  Code modified from https://elixirforum.com/t/how-to-test-live-views-with-ash-authentication-plugs/59814/
 
+  Thanks @peterhartman and @brunoripa
+  """
+  use PanicWeb.ConnCase, async: true
+  # import Phoenix.LiveViewTest
+
+  describe "user IS logged in" do
+    setup [:create_and_sign_in_user]
+
+    test "and their email shows in the top-right", %{conn: conn, user: user} do
       conn
-      |> visit("/sign-in")
-      |> fill_in("Email", with: user.email)
-      |> fill_in("Password", with: password)
-      |> click_button("Sign in")
-      |> assert_has("body", text: "Panic is live.")
+      |> visit("/")
+      |> assert_has("#current-user-email", text: user.email |> Ash.CiString.value())
+
+      # open_browser(view)
     end
+  end
+
+  describe "user is NOT logged in" do
+    test "and there's no email in the top-right", %{conn: conn} do
+      conn
+      |> visit("/")
+      |> refute_has("#current-user-email")
+    end
+  end
+
+  defp create_and_sign_in_user(%{conn: conn}) do
+    password = "abcd1234"
+    user = Panic.Fixtures.user(password)
+    strategy = AshAuthentication.Info.strategy!(Panic.Accounts.User, :password)
+
+    assert {:ok, user} =
+             AshAuthentication.Strategy.action(strategy, :sign_in, %{
+               email: user.email,
+               password: password
+             })
+
+    %{
+      conn:
+        conn
+        |> Phoenix.ConnTest.init_test_session(%{})
+        |> AshAuthentication.Plug.Helpers.store_in_session(user),
+      user: user
+    }
   end
 end
