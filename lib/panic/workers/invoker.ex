886bd86e174804b36d00de46e8e5b44b05ca8730
Ben Swift <ben@benswift.me>
2024-08-26 09:46:32 +1000

    add Invoker Oban job module
    this was partially generated by the Zed assistant, so need to check it
    
    but need to update the test harness a bit first

--- lib/panic/workers/invoker.ex
+++ lib/panic/workers/invoker.ex
@@ -1,8 +1,39 @@
 defmodule Panic.Workers.Invoker do
   use Oban.Worker, queue: :default
 
+  alias Panic.Engine
+
   @impl Oban.Worker
-  def perform(%Oban.Job{args: %{"id" => _id}}) do
-    :ok
+  def perform(%Oban.Job{args: %{"invocation_id" => invocation_id}}) do
+    with {:ok, invocation} <- Engine.get_invocation(invocation_id),
+         {:ok, network} <- Engine.get_network(invocation.network_id),
+         {:ok, invoked_invocation} <- Panic.Engine.invoke(invocation),
+         :ok <- maybe_schedule_next_invocation(network, invoked_invocation) do
+      :ok
+    end
+  end
+
+  defp maybe_schedule_next_invocation(network, invocation) do
+    if network.state in [:running, :starting] do
+      case Engine.prepare_next(previous_invocation: invocation) do
+        {:ok, next_invocation} ->
+          schedule_next_invocation(next_invocation)
+
+        {:error, reason} ->
+          {:error, reason}
+      end
+    else
+      :ok
+    end
+  end
+
+  defp schedule_next_invocation(next_invocation) do
+    %{invocation_id: next_invocation.id}
+    |> __MODULE__.new()
+    |> Oban.insert()
+    |> case do
+      {:ok, _job} -> :ok
+      {:error, reason} -> {:error, reason}
+    end
   end
 end
