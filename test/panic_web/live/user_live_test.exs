004fb557258a87be1e2618cebea12f46a1b0dc08
Ben Swift <ben@benswift.me>
2024-09-03 13:53:00 +1000

    add new UI test

--- test/panic_web/live/user_live_test.exs
+++ test/panic_web/live/user_live_test.exs
@@ -34,19 +34,35 @@ defmodule PanicWeb.UserLiveTest do
       |> click_link("#users-#{user.id} > td:nth-child(1)", Integer.to_string(user.id))
     end
 
-    test "and can visit the user index page and add an API token", %{
+    test "and can visit their user page and add an API token", %{
       conn: conn,
       user: user
     } do
       token_value = ";laskdfjhnlkjlkj"
 
       conn
-      |> visit("/users")
-      |> click_link("#users-#{user.id} > td:nth-child(1)", Integer.to_string(user.id))
+      |> visit("/users/#{user.id}")
       |> click_link("Update API Tokens")
       |> fill_in("Replicate API Token", with: token_value)
       |> submit()
       |> assert_has("#token-list", text: token_value)
+    end
+
+    test "and can visit their user page and create a network", %{
+      conn: conn,
+      user: user
+    } do
+      network_name = "My sweet network."
+      network_description = "Isn't it awesome?"
+
+      conn
+      |> visit("/users/#{user.id}")
+      |> click_link("Add network")
+      |> fill_in("Name", with: network_name)
+      |> fill_in("Description", with: network_description)
+      |> submit()
+      |> assert_has("#network-list", text: network_name)
+      |> assert_has("#network-list", text: network_description)
     end
   end
 
