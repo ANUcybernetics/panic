416a161df5fb07dd02b18a1928efe592dbbc89c0
Ben Swift <ben@benswift.me>
2024-09-05 21:55:41 +1000

    add some PhoenixTests for network live

---
+++ test/panic_web/live/network_live_test.exs
@@ -0,0 +1,27 @@
+defmodule PanicWeb.NetworkLiveTest do
+  use PanicWeb.ConnCase, async: true
+  use ExUnitProperties
+  # import Phoenix.LiveViewTest
+
+  describe "user IS logged in" do
+    setup {PanicWeb.Helpers, :create_and_sign_in_user}
+
+    test "and can create a network which is then listed on user page", %{conn: conn, user: user} do
+      conn
+      |> visit("/users/#{user.id}")
+      |> click_link("Add network")
+      |> fill_in("Name", with: "Test network")
+      |> fill_in("Description", with: "A network for testing purposes")
+      |> submit()
+      |> assert_has("#network-list", text: "Test network")
+    end
+
+    test "and can visit the terminal for a runnable network", %{conn: conn, user: user} do
+      # TODO currently I can't get PhoenixTest to fill out the LiveSelect yet, so fake it for now
+      network = Panic.Generators.network_with_models(user) |> pick()
+
+      conn
+      |> visit("/networks/#{network.id}/terminal")
+    end
+  end
+end
