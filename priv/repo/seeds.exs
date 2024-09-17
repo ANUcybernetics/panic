b43baccb07cb1a6587f8f62f7063fc90e3d39a58
Ben Swift <ben@benswift.me>
2024-09-18 09:20:36 +1000

    update the seeds file to create a user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# if there's no cybernetics user, create one
+case Ash.get(Panic.Accounts.User, %{email: "socy@anu.edu.au"}) do
+  {:error, _} ->
+    pass = Application.get_env(:panic, :socy_user_pass)
+
+    user =
+      Panic.Accounts.User
+      |> Ash.Changeset.for_create(
+        :register_with_password,
+        %{email: "socy@anu.edu.au", password: pass, password_confirmation: pass}
+      )
+      |> Ash.create!()
+
+    # add the tokens from the secrets file
+    Application.get_env(:panic, :api_tokens)
+    |> Enum.map(fn {name, value} ->
+      Panic.Accounts.set_token!(user, name, value, actor: user)
+    end)
+end
