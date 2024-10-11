d078137af63116db26823bdfc1d81b6ba1ac01e6
Ben Swift <ben@benswift.me>
2024-10-11 11:54:11 +1100

    don't render the list of links anymore
    the new redirect functionality is much nicer

--- lib/panic_web/live/network_live/display.ex
+++ lib/panic_web/live/network_live/display.ex
@@ -12,21 +12,6 @@ defmodule PanicWeb.NetworkLive.Display do
   import PanicWeb.PanicComponents
 
   @impl true
-  def render(%{live_action: :redirect} = assigns) do
-    ~H"""
-    <div>
-      <ul class="flex flex-col gap-12 m-16">
-        <%= for i <- 0..7 do %>
-          <.link navigate={~p"/networks/#{@network.id}/display/single/#{i}/8/"}>
-            <li class="h-16 bg-purple-900 flex items-center justify-center">Screen <%= i + 1 %></li>
-          </.link>
-        <% end %>
-      </ul>
-    </div>
-    """
-  end
-
-  @impl true
   def render(assigns) do
     ~H"""
     <p :if={@live_action == :grid} class="px-4 pt-2 pb-4">
