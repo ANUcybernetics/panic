697f279805d5f944963c21612538b4b59ab5b788
Ben Swift <ben@benswift.me>
2024-10-11 14:18:46 +1100

    audio invocation display: just use a picture of a waveform
    safer until we get the rpis hooked up - seemed to crash the TV

--- lib/panic_web/components/panic_components.ex
+++ lib/panic_web/components/panic_components.ex
@@ -206,16 +206,25 @@ defmodule PanicWeb.PanicComponents do
     """
   end
 
+  # def invocation_slot(%{type: :audio} = assigns) do
+  #   ~H"""
+  #   <div
+  #     class={["relative size-full", io_colour_mapper(:audio)]}
+  #     id={"#{@id}-audio-visualizer"}
+  #     phx-hook="AudioVisualizer"
+  #     data-audio-src={@value}
+  #   >
+  #     <div class="visualizer-container absolute inset-0"></div>
+  #   </div>
+  #   """
+  # end
+
   def invocation_slot(%{type: :audio} = assigns) do
     ~H"""
-    <div
-      class={["relative size-full", io_colour_mapper(:audio)]}
-      id={"#{@id}-audio-visualizer"}
-      phx-hook="AudioVisualizer"
-      data-audio-src={@value}
-    >
-      <div class="visualizer-container absolute inset-0"></div>
-    </div>
+    <img
+      class="object-cover w-full"
+      src="https://fly.storage.tigris.dev/panic-invocation-outputs/audio-waveform-image.webp"
+    />
     """
   end
 
