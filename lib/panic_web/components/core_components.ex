1e1cf6b29db397acd7aa25140329322659eab9ea
Ben Swift <ben@benswift.me>
2024-10-12 17:41:50 +1100

    jazz up the border & ring on the input["text"]
    especially nice for the terminal view

--- lib/panic_web/components/core_components.ex
+++ lib/panic_web/components/core_components.ex
@@ -383,7 +383,8 @@ defmodule PanicWeb.CoreComponents do
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
           "mt-2 block w-full rounded-lg text-purple-300 bg-zinc-800 focus:ring-0 sm:text-sm sm:leading-6",
-          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          "phx-no-feedback:border-zinc-700 phx-no-feedback:focus:border-purple-500",
+          "phx-no-feedback:focus:ring-purple-300 phx-no-feedback:focus:ring-2",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
