7472546cea9983d50e993a06bbb4fdd219550a01
Ben Swift <ben@benswift.me>
2024-10-12 21:33:29 +1100

    add "chrome launcher" shell script
    this will be copied over to the mac mini

---
+++ open_chrome_kiosk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Open Chrome in kiosk mode and navigate to the specified URL
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --kiosk "https://panic.fly.dev/networks/5/terminal"
