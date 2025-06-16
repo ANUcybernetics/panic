4dec9d0ba1a89d814cf8cfab243b119e32e73bc5
Ben Swift <ben@benswift.me>
2025-06-16 21:40:40 +1000

    improve add/remove watcher change modules

--- lib/panic/engine/changes/add_watcher.ex
+++ lib/panic/engine/changes/add_watcher.ex
@@ -13,9 +13,19 @@ defmodule Panic.Engine.Changes.AddWatcher do
 
   @impl true
   def change(changeset, _opts, _context) do
-    current_watchers = Ash.Changeset.get_attribute(changeset, :watchers) || []
+    current_watchers = Ash.Changeset.get_attribute(changeset, :watchers)
     watcher = Ash.Changeset.get_argument(changeset, :watcher)
-    new_watchers = current_watchers ++ [watcher]
-    Ash.Changeset.change_attribute(changeset, :watchers, new_watchers)
+
+    case {current_watchers, watcher} do
+      {nil, _} ->
+        Ash.Changeset.add_error(changeset, field: :watchers, message: "watchers attribute is required")
+
+      {_, nil} ->
+        Ash.Changeset.add_error(changeset, field: :watcher, message: "watcher argument is required")
+
+      {watchers, watcher} ->
+        new_watchers = watchers ++ [watcher]
+        Ash.Changeset.change_attribute(changeset, :watchers, new_watchers)
+    end
   end
 end
