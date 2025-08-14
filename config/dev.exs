aadcd03e152487c12b2dd0da9de7034fce18735f
Ben Swift <ben@benswift.me>
2025-08-14 12:14:59 +1000

    set default local dev server port to 4000

--- config/dev.exs
+++ config/dev.exs
@@ -23,10 +23,13 @@ config :panic, Panic.Repo,
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
+# Get port from environment variable or use default
+http_port = String.to_integer(System.get_env("PHX_PORT") || "4010")
+
 config :panic, PanicWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: http_port],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
