b9927824fcd7c323b0f38b839869a9f0d01f8f79
Ben Swift <ben@benswift.me>
2025-08-18 16:16:49 +1000

    add md to static_paths

--- lib/panic_web.ex
+++ lib/panic_web.ex
@@ -17,7 +17,7 @@ defmodule PanicWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt md)
 
   def router do
     quote do
