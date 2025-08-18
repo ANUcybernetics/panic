3c6e16cdc6cc556de45923a06f1ddf824f9a37fd
Ben Swift <ben@benswift.me>
2025-08-18 16:53:45 +1000

    remove some more about config

--- lib/panic_web.ex
+++ lib/panic_web.ex
@@ -17,7 +17,7 @@ defmodule PanicWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt md)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
   def router do
     quote do
