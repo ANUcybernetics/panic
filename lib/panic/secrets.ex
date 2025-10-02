08d41785b9f24b0bf9a3298b9ce37f990f56ff2b
Ben Swift <ben@benswift.me>
2025-10-02 22:22:12 +1000

    revert hacky CI fixes - will investigate properly

--- lib/panic/secrets.ex
+++ lib/panic/secrets.ex
@@ -3,17 +3,6 @@ defmodule Panic.Secrets do
   use AshAuthentication.Secret
 
   def secret_for([:authentication, :tokens, :signing_secret], Panic.Accounts.User, _opts, _context) do
-    case Application.fetch_env(:panic, :token_signing_secret) do
-      {:ok, secret} ->
-        {:ok, secret}
-
-      :error ->
-        # Fallback for test environment if config isn't loaded yet
-        if Mix.env() == :test do
-          {:ok, "lR3r6rkW8nRkChM35qcKl00FNSK95ra5"}
-        else
-          :error
-        end
-    end
+    Application.fetch_env(:panic, :token_signing_secret)
   end
 end
