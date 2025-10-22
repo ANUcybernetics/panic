b1e1689f53d19ac7d1eb61a6e1b33f6af6dbff52
Ben Swift <ben@benswift.me>
2025-10-22 11:01:26 +1100

    update version to 3.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Panic.MixProject do
   def project do
     [
       app: :panic,
-      version: "3.0.0-beta.0",
+      version: "3.0.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
